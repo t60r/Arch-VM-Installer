977a13a94fe425af31f43858651d730eb27d8e48
t60r <merkinmaker@gmail.com>
2014-04-26 10:57:23 -0500

    added a file

---
+++ vboxstuff.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Date: 4/26/14
+# Author: merkinmaker
+# Purpose: Vbox Modules for Much VM
+#
+# Depends: Arch Linux Live ISO, prechroot.sh, postchroot.sh
+#
+# **This entire project is a WIP
+#####################################
+
+vboxthings() {
+	pacman -S virtualbox-guest-utils virtualbox-guest-dkms --noconfirm
+	modprobe vboxdrv vboxsf vboxvideo
+	systemctl enable dkms.service
+}
+
+main () {
+ vboxthings
+}
+
+main
+
+#END
