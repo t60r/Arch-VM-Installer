ce9859de1fd3fdc2924058cf785b1b1cd8b8a96a
t60r <james.roe.jr@gmail.com>
2014-04-28 15:07:18 -0500

    Update vboxstuff.sh

--- vboxstuff.sh
+++ vboxstuff.sh
@@ -13,17 +13,19 @@ white=$(tput setaf 7)
 green=$(tput setaf 2)
 yellow=$(tput setaf 3)
 
-
-vboxthings() {
+packages() {
 	printf " \033[1m ${red} Downloading and setting up vbox utils and modules. \n \033[0m "
 	sleep 3
-	pacman -S virtualbox-guest-utils virtualbox-guest-dkms virtualbox-guest-modules --noconfirm
+	pacman -S virtualbox-guest-utils virtualbox-guest-dkms virtualbox-guest-modules --noconfirm	
+}
+vboxthings() {
 	systemctl enable dkms.service
 	systemctl start dkms.service
 	modprobe -a vboxguest vboxsf vboxvideo
 }
 
 main () {
+	packages
 	vboxthings
 }
 
